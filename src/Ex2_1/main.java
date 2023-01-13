55ada1bad63bb1757c5b9209f1eb692a5d089940
AvivTurgeman <aaviiv@gmail.com>
2023-01-13 06:41:22 +0200

    not a segnificate change

--- src/Ex2_1/main.java
+++ src/Ex2_1/main.java
@@ -3,13 +3,14 @@ package src.Ex2_1;
 public class main {
     public static void main(String[] args) {
         long start, end;
-        int filesAmount = 100;
+        int filesAmount = 10000;
         int seed = 4;
         int bound = 100;
         int noThreads, oneThread, ThreadPool;
 
 
         String[] fileNames = Ex2_1.createTextFiles(filesAmount, seed, bound);
+        System.out.println(filesAmount + " created");
 
         start = System.currentTimeMillis();
         noThreads = Ex2_1.getNumOfLines(fileNames);
@@ -27,8 +28,5 @@ public class main {
         System.out.println("ThreadPool time: " + (end-start) + "ms");
 
         System.out.println("\nNo Threads Lines amount: " + noThreads + "\n1 Thread Lines amount:   " + oneThread + "\nThreadPool Lines amount: " + ThreadPool);
-
-
-
     }
 }
